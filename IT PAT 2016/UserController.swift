3def06d17a333da8b14e03b78d3b36dbda13b3f7
Stephan Cilliers <stephanus.cilliers@gmail.com>
2016-10-20 12:04:35 +0200

    Added ActivityIndicator class and implemented into handleLogin and handleRegister

--- IT PAT 2016/UserController.swift
+++ IT PAT 2016/UserController.swift
@@ -11,6 +11,7 @@ import Firebase
 class UserController {
 	var currentUser: FIRUser?
 	var loginViewController: UIViewController?
+	var activityIndicator: ActivityIndicator?
 	
 	func logout() {
 		try! FIRAuth.auth()?.signOut()
@@ -24,10 +25,12 @@ class UserController {
 	}
 	
 	func handleLogin(email: String, password: String) {
+		activityIndicator?.show()
 		FIRAuth.auth()?.signIn(withEmail: email, password: password) { (user, error) in
 			if error != nil {
 				print("Error: \(error.debugDescription)")
 				alertUser(viewController: self.loginViewController!)
+				self.activityIndicator?.hide()
 			} else {
 				self.currentUser = user
 				if self.currentUser?.displayName == nil {
@@ -38,6 +41,7 @@ class UserController {
 					self.updateLocalLogin(email: email, password: password)
 					// Take user to main menu
 					self.loginViewController?.performSegue(withIdentifier: "LoginToMenu", sender: self.loginViewController)
+					
 				}
 			}
 		}
@@ -45,6 +49,7 @@ class UserController {
 	}
 	
 	func handleRegister(email: String, password: String) {
+		activityIndicator?.show()
 		FIRAuth.auth()?.createUser(withEmail: email, password: password) { (user, error) in
 			if error != nil {
 				if let errCode = FIRAuthErrorCode(rawValue: error!._code) {
@@ -53,8 +58,8 @@ class UserController {
 						alertUser(viewController: self.loginViewController!, message: "Email already in use.")
 					default:
 						alertUser(viewController: self.loginViewController!)
-						
 					}
+					self.activityIndicator?.hide()
 				}
 			} else {
 				// Remeber user login cridentials
@@ -62,6 +67,57 @@ class UserController {
 				uc.updateLocalLogin(email: email, password: password)
 				self.loginViewController?.performSegue(withIdentifier: "LoginToRegister", sender: self.loginViewController)
 			}
+		}
+	}
+	
+	/* UI */
+	
+}
+
+
+class ActivityIndicator: UIView {
+	var spinner = UIActivityIndicatorView(activityIndicatorStyle: .whiteLarge)
+	var loadingView: UIView = UIView()
+	var parentView: UIView?
+	
+//	override init (frame : CGRect) {
+//		super.init(frame : frame)
+//	}
+	
+	required init(coder aDecoder: NSCoder) {
+		fatalError("This class does not support NSCoding")
+	}
+	
+	init (parentView: UIView){
+		super.init(frame: CGRect.zero)
+		self.parentView = parentView
+	}
+	
+	func show() {
+		DispatchQueue.main.async {
+			self.loadingView = UIView()
+			self.loadingView.frame = CGRect(x: 0.0, y: 0.0, width: 100.0, height: 100.0)
+			self.loadingView.center = (self.parentView?.center)!
+			//		loadingView.backgroundColor = UIColor(rgba: "#444444")
+			self.loadingView.backgroundColor = UIColor.lightGray
+			self.loadingView.alpha = 0.7
+			self.loadingView.clipsToBounds = true
+			self.loadingView.layer.cornerRadius = 10
+			
+			self.spinner = UIActivityIndicatorView(activityIndicatorStyle: .whiteLarge)
+			self.spinner.frame = CGRect(x: 0.0, y: 0.0, width: 80.0, height: 80.0)
+			self.spinner.center = CGPoint(x:self.loadingView.bounds.size.width / 2, y:self.loadingView.bounds.size.height / 2)
+			
+			self.loadingView.addSubview(self.spinner)
+			self.parentView?.addSubview(self.loadingView)
+			self.spinner.startAnimating()
+		}
+	}
+	
+	func hide() {
+		DispatchQueue.main.async {
+			self.spinner.stopAnimating()
+			self.loadingView.removeFromSuperview()
 		}
 	}
 }
