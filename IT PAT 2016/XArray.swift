ef928da6a991716badc8d03b93c67f51b41eba6d
Stephan Cilliers <stephanus.cilliers@gmail.com>
2016-11-01 13:37:12 +0200

    Added files

---
+++ IT PAT 2016/XArray.swift
@@ -0,0 +1,49 @@
+//
+//  XArray.swift
+//  IT PAT 2016
+//
+//  Created by Stephan Cilliers on 2016/10/31.
+//  Copyright © 2016 Stephan Cilliers. All rights reserved.
+//
+
+import SpriteKit
+
+class XArray: SKNode {
+	
+	var nodes: [SKSpriteNode]
+	
+	init(Xs: Int, spacing: CGFloat) {
+		nodes = []
+		super.init()
+		
+		let firstSprite = SKSpriteNode(imageNamed: "incorrect")
+		firstSprite.position = CGPoint(x: 0, y: 0)
+		nodes.append(firstSprite)
+		
+		for i in 0...Xs {
+			let sprite = SKSpriteNode(imageNamed: "incorrect")
+			sprite.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+			sprite.position = CGPoint(x: nodes[i].position.x+spacing*2, y: 0)
+			nodes.append(sprite)
+		}
+		
+		for node in nodes {
+			node.isHidden = true
+			node.setScale(0.05)
+			addChild(node)
+		}
+	}
+	
+	func displayNext() {
+		for node in nodes {
+			if node.isHidden {
+				node.isHidden = false
+				break
+			}
+		}
+	}
+	
+	required init?(coder aDecoder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+}
